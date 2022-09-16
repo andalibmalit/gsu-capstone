938632993d4376fa758dfd9f9a3dd83f61c6e61f
andalibmalit <andalibmalit@gmail.com>
2022-09-15 23:46:09 -0400

    resolving merge conflict

--- tearing-me-apart.py
+++ tearing-me-apart.py
@@ -1,5 +1,4 @@
 print("Hello world! My favorite movie is The Room")
 # (Replace "The Room" with your actual favorite movie.)
-<<<<<<< HEAD
 print("My favorite song is Hungarian Rhapsody no. 2 by Franz List")
 # You monster, you made me choose betweent this and Touch by Daft Punk...
